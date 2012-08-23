f1f383093536bedc6d715722d42be6753c6ef716
stef <stefan.marsiske@gmail.com>
2012-08-23 15:29:30 +0200

    [enh] minor tweaks and urldecoding

--- alimit.sh
+++ alimit.sh
@@ -41,6 +41,25 @@ do
 done
 shift `expr $OPTIND - 1`
 
+function urldecode {
+    arg="$1"
+    i=0
+    while [[ $i -lt ${#arg} ]]; do
+        p0=${arg:$i:1}
+        if [ "x$p0" = "x%" ]; then
+            #p1=${arg:$((i+1)):1}
+            #p2=${arg:$((i+2)):1}
+            p=${arg:$((i+1)):2}
+            printf "\x$p"
+            #printf "\x$p1$p2"
+            i=$((i+3))
+        else
+            echo -n $p0
+            i=$((i+1))
+        fi
+    done
+}
+
 tail -F -n 0 "$logfile" | while read entry; do
     # only handle 200 OK responses
     error=$(print "$entry" | sed 's/.*\] "[^"]*" \([0-9]*\).*/\1/')
@@ -49,9 +68,9 @@ tail -F -n 0 "$logfile" | while read entry; do
     # extract path and filename from request
     req=$(print "$entry" | sed -r 's/.*]\s"([^ ]+)\s.*/\1/')
     [[ "x$req" != "xGET" && "x$req" != "xPOST" ]] && continue
-    rpath=$(print "$entry" | sed -r 's/.*] "'$req' (.*) HTTP\/[0-9.]*" .*/\1/')
+    rpath=$(urldecode "$entry" | sed -r 's/.*] "'$req' (.*) HTTP\/[0-9.]*" .*/\1/')
 
-    [[ -f "$docroot${rpath##$skippath}" ]] || {
+    [[ -e "$docroot${rpath##$skippath}" ]] || {
         print "$(date --rfc-3339=ns) WARN not found: $rpath $entry"
         continue
     }
@@ -63,7 +82,7 @@ tail -F -n 0 "$logfile" | while read entry; do
     [[ -r "$limitfile" ]] && {
         size=$(print "$entry" | sed 's/.*\] "[^"]*" [0-9]* \([0-9]*\).*/\1/')
         # ignore incomplete downloads
-        [[ "$size" -lt $(stat -c "%s" "$docroot$directory$filename") ]] && continue
+        [[ "$size" -lt $(stat -c "%s" "$docroot$directory/$filename") ]] && continue
         ttl=$(( $(head -1 "$limitfile") - 1))
         [[ "$ttl" -gt 0 ]] && {
             # alimited file hit, decrease ttl
@@ -73,7 +92,7 @@ tail -F -n 0 "$logfile" | while read entry; do
         [[ "$ttl" -eq 0 ]] && {
             # ttl exceeded wipe file from disk
             tlimit="$docroot$directory/.$filename.tlimit"
-            srm -fll "$docroot$directory$filename" "$limitfile" "$tlimit";
+            srm -fll "$docroot$directory/$filename" "$limitfile" "$tlimit" 2>/dev/null;
             print "$(date --rfc-3339=ns) KILL $rpath $entry"
         }
     }
